10cd411b82a38cf11dfba0de95cfed07741215e8
vishnu2812 <101513111+vishnu2812@users.noreply.github.com>
2024-04-15 12:31:20 -0700

    Delete server directory

--- server/config/database.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  const { connection } = await mongoose.connect("mongodb://127.0.0.1:27017/razor");
-  console.log(`Mongodb is connected with ${connection.host}`);
-};
